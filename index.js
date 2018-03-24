eec937f53f7e6e0d3d71d424c537433f1f2968c0
kumardastan <kumardastan@gmail.com>
2018-03-25 02:41:06 +0600

    feat: implement modal loader

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Modal,
+  ActivityIndicator
+} from 'react-native';
+
+export const Loader = ({ loading }) => {
+  return (
+    <Modal
+      transparent
+      animationType={'none'}
+      visible={loading}
+    >
+      <View style={styles.modalBackground}>
+        <View style={styles.activityIndicatorWrapper}>
+          <ActivityIndicator animating={loading} />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)'
+  },
+  activityIndicatorWrapper: {
+    backgroundColor: 'white',
+    height: 100,
+    width: 100,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
